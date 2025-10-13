4146e4a1988f859fbe0ad384b33c5a2a6df2fc64
evaldhio <117887739+evaldhio@users.noreply.github.com>
2025-10-13 23:45:44 +0700

    Update sirion.sh

--- sirion.sh
+++ sirion.sh
@@ -3,10 +3,10 @@ iface eth0 inet static
     address 10.89.3.10
     netmask 255.255.255.0
     gateway 10.89.3.1
-    up echo nameserver 192.168.122.1 > /etc/resolv.conf
-
-    ### No. 11
-    apt-get update
+    up echo nameserver 192.168.122.1 > /etc/resolv.conf 
+    
+### No. 11
+apt-get update
 
 apt-get install nginx -y
 
